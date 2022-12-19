c161cd50e1bc258b5a3051488074fd099da3a54b
taku1093 <pokettoassi@icloud.com>
2022-12-19 17:10:17 +0900

    シミュレーション関連追加

---
+++ wacs_laravel/app/Http/Controllers/SimulationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SimulationController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('simulation.simulation_list');
+    }
+}
---
+++ wacs_laravel/resources/views/simulation/simulation_list.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+
+@section('content')
+<!DOCTYPE html>
+    <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+        <link rel="stylesheet" type="text/css" href="./css/sim_slide.css">
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+
+    <p style="text-align:center" class="backgroundImage moji" >シミュレーション</p>
+    
+
+    <!-- スライド機能   -->
+    {{--  椅子  --}}
+    <h2 style="text-align:center">Chair</h2>
+    <ul class="slider">
+        <li><a href="simu_test.html"><img  src="img/simulation/sikakuisu.jpg" alt=""></a></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+    </ul>
+
+    {{--  机  --}}
+    <h2 style="text-align:center">Desk</h2>
+    <ul class="slider">
+        <li><a href="simu_test2.html"><img  src="img/simulation/desk.jpg" alt=""></a></li>
+        <li><img  src="img/simulation/desk.jpg" alt=""></li>
+        <li><img  src="img/simulation/desk.jpg" alt=""></li>
+        <li><img  src="img/simulation/desk.jpg" alt=""></li>
+        <li><img  src="img/simulation/desk.jpg" alt=""></li>
+        <li><img  src="img/simulation/desk.jpg" alt=""></li>
+        <li><img  src="img/simulation/desk.jpg" alt=""></li>
+        <li><img  src="img/simulation/desk.jpg" alt=""></li>
+    </ul>
+
+    <!-- 棚 -->
+    <h2 style="text-align:center">Shelf</h2>
+    <ul class="slider">
+        <li><a href="simu_test.html"><img  src="img/simulation/sikakuisu.jpg" alt=""></a></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+    </ul>
+
+    <!-- その他 -->
+    <h2 style="text-align:center">Onothr</h2>
+    <ul class="slider">
+        <li><a href="simu_test.html"><img  src="img/simulation/sikakuisu.jpg" alt=""></a></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+        <li><img  src="img/simulation/sikakuisu.jpg" alt=""></li>
+    </ul>
+    
+     <!-- <a href="simu_test.html">
+        <img  src="img/simulation/sikakuisu.jpg" alt="次へ">
+     </a> -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <!--自作のJS-->
+    <script src="./js/sim_slide.js"></script>
+    
+    </body>
+    </html>
+
+    @endsection

f8b57ba43eb42efaadb9d81237c4dde1adf70b17
taku1093 <pokettoassi@icloud.com>
2022-12-30 14:25:59 +0900

    コード簡易化

--- wacs_laravel/app/Http/Controllers/PostsController.php
+++ wacs_laravel/app/Http/Controllers/PostsController.php
@@ -59,41 +59,29 @@ class PostsController extends Controller
     {
         $user = auth()->user();
         $data = $request->all();
+        $validator = Validator::make($data, [
+            'post_title' => ['required', 'string', 'max:140'],
+            'post_exp' => ['required', 'string', 'max:140'],
+            'method' => ['required', 'string', 'max:140'],
+        ]);
         if ($request->has("post_img3")) {
-            // 画像二枚保存時
+            // 画像3枚保存時
             $validator = Validator::make($data, [
-                'post_title' => ['required', 'string', 'max:140'],
                 'post_img1' => ['file',  'mimes:jpeg,png,jpg', 'max:2048'],
                 'post_img2' => ['file',  'mimes:jpeg,png,jpg', 'max:2048'],
                 'post_img3' => ['file',  'mimes:jpeg,png,jpg', 'max:2048'],
-                'post_exp' => ['required', 'string', 'max:140'],
-                'method' => ['required', 'string', 'max:140'],
-                // 'material_name' => ['required', 'string', 'max:140'],
-                // 'tool' => ['required', 'string', 'max:140'],
-                // 'post_tag'=> ['required', 'string', 'max:140']
             ]);
-        }elseif($request->has("post_img2")) {
-            // 画像3枚保存時
+        }
+        if($request->has("post_img2")) {
+            // 画像2枚保存時
             $validator = Validator::make($data, [
-                'post_title' => ['required', 'string', 'max:140'],
                 'post_img1' => ['file',  'mimes:jpeg,png,jpg', 'max:2048'],
                 'post_img2' => ['file',  'mimes:jpeg,png,jpg', 'max:2048'],
-                'post_exp' => ['required', 'string', 'max:140'],
-                'method' => ['required', 'string', 'max:140'],
-                // 'material_name' => ['required', 'string', 'max:140'],
-                // 'tool' => ['required', 'string', 'max:140'],
-                // 'post_tag'=> ['required', 'string', 'max:140']
             ]);
-        }else{
+        }if($request->has("post_img1")){
             // 画像1枚保存時
             $validator = Validator::make($data, [
-                'post_title' => ['required', 'string', 'max:140'],
                 'post_img1' => ['file',  'mimes:jpeg,png,jpg', 'max:2048'],
-                'post_exp' => ['required', 'string', 'max:140'],
-                'method' => ['required', 'string', 'max:140'],
-                // 'material_name' => ['required', 'string', 'max:140'],
-                // 'tool' => ['required', 'string', 'max:140'],
-                // 'post_tag'=> ['required', 'string', 'max:140']
             ]);
         }
         // 材料

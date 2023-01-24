8789690fb24319d8c865e6aad767a60cadd47e85
yuma5340 <240315w@ugs.kochi-tech.ac.jp>
2023-01-24 16:54:06 +0900

    修正

--- wacs_laravel/resources/views/qanda/create.blade.php
+++ wacs_laravel/resources/views/qanda/create.blade.php
@@ -14,7 +14,7 @@
 <div class="container mt-4">
     <div class="border p-4">
         <h1 class="h4 mb-4 font-weight-bold">
-            投稿の新規作成
+            質問の新規作成
         </h1>
  
         <form method="POST" action="{{ route('qanda.store') }}">
